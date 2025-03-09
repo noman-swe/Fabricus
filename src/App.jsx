191bfb268f4fa50acc64d033c73d5f227ed9f85f
noman <noman.swe130@gmail.com>
2025-03-10 01:04:52 +0600

    [added Navbar with pop-up order and dark and light mode feature]

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Popup from "./components/Popup/Popup";
+
 const App = () => {
+  const [orderPopup, setOrderPopup] = React.useState(false);
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
-      App
+      <Navbar handleOrderPopup={handleOrderPopup} />
+
+      <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
     </div>
   );
 };
