137128ddc7c7740b1f9a75c45a0c8e66db18d337
noman <noman.swe130@gmail.com>
2025-03-10 05:01:15 +0600

    [products added]

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Navbar from "./components/Navbar/Navbar";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Popup from "./components/Popup/Popup";
-import Hero from "./components/BannerSlider/BannerSlider";
 import BannerSlider from "./components/BannerSlider/BannerSlider";
+import Products from "./components/Products/Products";
 
 const App = () => {
   const [orderPopup, setOrderPopup] = React.useState(false);
@@ -26,6 +26,8 @@ const App = () => {
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
       <Navbar handleOrderPopup={handleOrderPopup} />
       <BannerSlider handleOrderPopup={handleOrderPopup} />
+
+      <Products />
       <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
     </div>
   );
