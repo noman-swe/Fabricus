7d81a322ea91b28d71e8e899bbb9da803dc1a3f0
noman <noman.swe130@gmail.com>
2025-03-10 01:30:13 +0600

    [slider designed with react-slick]

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar/Navbar";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Popup from "./components/Popup/Popup";
+import Hero from "./components/BannerSlider/BannerSlider";
+import BannerSlider from "./components/BannerSlider/BannerSlider";
 
 const App = () => {
   const [orderPopup, setOrderPopup] = React.useState(false);
@@ -23,7 +25,7 @@ const App = () => {
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
       <Navbar handleOrderPopup={handleOrderPopup} />
-
+      <BannerSlider handleOrderPopup={handleOrderPopup} />
       <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
     </div>
   );
